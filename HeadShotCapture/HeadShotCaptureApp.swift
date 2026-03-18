00e515e08b9eac9be45955e75ffcb6daba6dc6ad
ari <ariteverett@gmail.com>
2026-03-18 10:49:02 -0700

    Initial Commit

---
+++ HeadShotCapture/HeadShotCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadShotCaptureApp.swift
+//  HeadShotCapture
+//
+//  Created by Ari Everett on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct HeadShotCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
