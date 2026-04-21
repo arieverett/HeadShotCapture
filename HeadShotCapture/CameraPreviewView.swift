c2ea73d7e6572a396e8facb23dd7cc33ad2cf7fe
ari <ariteverett@gmail.com>
2026-04-21 04:09:51 -0400

    updated commit for camerapreviewview

--- HeadShotCapture/CameraPreviewView.swift
+++ HeadShotCapture/CameraPreviewView.swift
@@ -2,7 +2,33 @@
 //  CameraPreviewView.swift
 //  HeadShotCapture
 //
-//  Created by Ari Everett on 3/30/26.
+//  Created by Ari Everett on 3/23/26.
 //
 
 import Foundation
+import SwiftUI
+import AVFoundation
+
+struct CameraPreviewView: UIViewRepresentable {
+    let session: AVCaptureSession
+    
+    func makeUIView(context: Context) -> some PreviewUIView {
+        let view = PreviewUIView()
+        view.previewLayer.session = session
+        view.previewLayer.videoGravity = .resizeAspectFill
+        return view
+    }
+    
+    func updateUIView(_ uiView: PreviewUIView, context: Context) {
+    }
+}
+
+class PreviewUIView: UIView {
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
+    }
+    
+    var previewLayer: AVCaptureVideoPreviewLayer {
+        layer as! AVCaptureVideoPreviewLayer
+    }
+}
--- HeadShotCapture/CaptureHandler.swift
+++ HeadShotCapture/CaptureHandler.swift
@@ -38,6 +38,7 @@ class CaptureHandler: NSObject {
         }
         
         configureSession()
+         
         devices = discoverCaptureDevices()
     }
     
